db71752c8ce3602c8546042ffe8b4454b2b29ec4
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-11 18:22:47 +0200

    move uuid list to separate component

---
+++ src/components/UuidList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function UuidList({itemOnList}) {
+  return (
+    <div>
+      {itemOnList && (
+        <div className="uuid-container">
+          <h2>UUID identifiers</h2>
+          <ul>{itemOnList}</ul>
+        </div>
+      )}
+    </div>
+  );
+}
