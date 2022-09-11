9459231f50b92a47fc6fc7e0e15fb872adbba7b0
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-11 18:23:51 +0200

    export components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as TopGenerator } from "./TopGenerator";
+export { default as Card } from "./Card";
+export { default as Footer } from "./Footer";
+export { default as UuidList } from "./UuidList";
+export { default as CopyButton } from "./CopyButton";
