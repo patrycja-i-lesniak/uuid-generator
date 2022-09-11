7148ff139ce8561c7cc02ab519222a30fce43eec
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-11 18:27:22 +0200

    rebuild App component, add handlers and states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+
+import { TopGenerator, Footer, Card, UuidList } from "./components";
 
 function App() {
+  const [uuid, setUuid] = useState(uuidv4());
+  const [items, setItems] = useState(0);
+  const [itemOnList, setItemOnList] = useState("");
+
+  function handleUuid() {
+    setUuid(uuidv4());
+  }
+
+  function handleItems(e) {
+    setItems(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setItemOnList(list);
+    e.target.reset();
+  }
+
+  function GenerateList() {
+    let itemList = [];
+    for (let i = 1; i <= items; i++) {
+      itemList.push(uuidv4());
+    }
+    return itemList;
+  }
+
+  const list = GenerateList().map((item, index) => {
+    return <li key={index}>{item}</li>;
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopGenerator uuid={uuid} handleUuid={handleUuid} />
+      <Card
+        uuid={uuid}
+        handleUuid={handleUuid}
+        handleSubmit={handleSubmit}
+        handleItems={handleItems}
+      />
+      <UuidList itemOnList={itemOnList} />
+      <Footer />
     </div>
   );
 }
