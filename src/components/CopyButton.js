82f9727298f8521f0dd230038fa24e26bda5f741
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-11 18:23:27 +0200

    create separate components

---
+++ src/components/CopyButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { AiOutlineCopy } from "react-icons/ai";
+
+export default function CopyButton({uuid}) {
+  return (
+    <div>
+      <CopyToClipboard text={uuid} onCopy={() => [alert("Copied uuid", { uuid })]}>
+        <button style={{ width: "100px" }}>
+          <AiOutlineCopy />
+          Copy{" "}
+        </button>
+      </CopyToClipboard>
+    </div>
+  );
+}
