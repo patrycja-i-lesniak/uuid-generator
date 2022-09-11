158a3bee0b50e0406552ef88f271c33d2f07f3c3
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-11 18:02:48 +0200

    move Card logic to separate component

---
+++ src/components/Card.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function Card({ uuid, handleUuid, handleSubmit, handleItems }) {
+  return (
+    <div className="card">
+      <h2 className="card-header"> Version 4UUID Generator</h2>
+      <div className="card-body">
+        <div className="card-button-container">
+          <button
+            className="button"
+            onClick={() => {
+              handleUuid();
+            }}
+          >
+            Generate a version 4 UUID
+          </button>
+        </div>
+
+        <h3>Bulk Version 4 UUID Generation</h3>
+
+        <div className="form-box">
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div className="form-inline">
+              <label className="form-label">How Many?</label>
+              <div className="input-group">
+                <input
+                  type="number"
+                  onChange={(e) => {
+                    handleItems(e);
+                  }}
+                  placeholder="Enter number"
+                />
+                <div className="input-group">
+                  <button type="submit">Generate</button>
+                </div>
+              </div>
+              {/* {itemOnList && (
+                  <a href="uuidList.txt" download>
+                    Download to a file
+                  </a>
+                )} */}
+            </div>
+          </form>
+          <div className="whatis">
+            <h3>What is a version 4 UUID?</h3>
+            <p>
+              A Version 4 UUID is a universally unique identifier that is generated using random
+              numbers. The Version 4 UUIDs produced by this site were generated using a secure
+              random number generator.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
