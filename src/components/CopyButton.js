52eace5bd4d5a001d620947d130e0e8ebbdcdf45
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-14 09:48:21 +0200

    import backdrop and popup components, add logic to manage popup visibility, set a timer to automatically turn off the popup after 5 seconds

--- src/components/CopyButton.js
+++ src/components/CopyButton.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { AiOutlineCopy } from "react-icons/ai";
+import { Popup, Backdrop } from "../components";
 
 export default function CopyButton({ uuid }) {
+  const [show, setShow] = useState(false);
+
+  function handleCopy(e) {
+    console.log("UUID", uuid, "has been copied:");
+    setShow(true);
+  }
+
+  function handleClose(e) {
+    e.preventDefault();
+    setShow(false);
+  }
+
+  function toggleShow() {
+    setShow(!show);
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShow(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [show]);
+
   return (
     <div>
-      <CopyToClipboard text={uuid} onCopy={() => [alert("Copied uuid", { uuid })]}>
-        <button style={{ width: "100px" }}>
+      {show && (
+        <>
+          <Popup handleClose={handleClose} uuid={uuid} />
+          <Backdrop className="backdrop" toggleShow={toggleShow} />
+        </>
+      )}
+
+      <CopyToClipboard
+        text={uuid}
+        onCopy={(e) => {
+          handleCopy(e);
+        }}
+      >
+        <button className="copy-button" style={{ width: "100px" }}>
           <AiOutlineCopy />
           Copy{" "}
         </button>
