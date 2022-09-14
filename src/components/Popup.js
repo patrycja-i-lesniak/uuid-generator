e2ab887c6c469029e2b425c50ce5af00bb424c37
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-14 10:27:04 +0200

    add close icon and style it

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,14 +1,17 @@
 import React from "react";
 
+import { MdClose } from "react-icons/md";
+
+
 export default function Popup({ uuid, handleClose }) {
   return (
     <div className="popup">
       <div>
+        <MdClose className='close' onClick={(e) => handleClose(e)} />
         <p>UUID</p>
         <p className="popup-uuid"> {uuid}</p>
         <p>has been copied</p>
       </div>
-
       <button className="popup-button" onClick={(e) => handleClose(e)}>
         close
       </button>
