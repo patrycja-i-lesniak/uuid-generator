e15b810a1899d5edecfddf3e768e3412fa575467
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-14 09:46:07 +0200

    add exports

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Card } from "./Card";
 export { default as Footer } from "./Footer";
 export { default as UuidList } from "./UuidList";
 export { default as CopyButton } from "./CopyButton";
+export { default as Popup } from "./Popup";
+export {default as Backdrop} from './Backdrop';
