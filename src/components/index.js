f4445e7dd6b309f442a2d5133431fc2398c0d81c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-14 21:44:35 +0200

    add new imports

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,6 @@ export { default as Footer } from "./Footer";
 export { default as UuidList } from "./UuidList";
 export { default as CopyButton } from "./CopyButton";
 export { default as Popup } from "./Popup";
-export {default as Backdrop} from './Backdrop';
+export { default as Backdrop } from "./Backdrop";
+export { default as Form } from "./Form";
+export { default as Button } from "./Button";
